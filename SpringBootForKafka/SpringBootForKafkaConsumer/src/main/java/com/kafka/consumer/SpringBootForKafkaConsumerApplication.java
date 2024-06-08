217efd7c44d2836af365efe889520652346b7233
Luis-Alberto-Espinoza <espinoza.luis.alberto1981@gmail.com>
2024-06-08 19:45:43 -0300

    Agregado del microservicio Consumer

---
+++ SpringBootForKafka/SpringBootForKafkaConsumer/src/main/java/com/kafka/consumer/SpringBootForKafkaConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.kafka.consumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootForKafkaConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootForKafkaConsumerApplication.class, args);
+	}
+
+}
