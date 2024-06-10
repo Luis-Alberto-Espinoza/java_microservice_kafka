46f08827bcafcba7dd9cde9ec6df1a6f4ba8a729
Luis-Alberto-Espinoza <espinoza.luis.alberto1981@gmail.com>
2024-06-10 12:38:40 -0300

    creacion del KafkaProviderConfig

--- SpringBootForKafka/SpringBootForKafkaProvider/src/main/java/com/kafka/provider/config/KafkaTopicConfig.java
+++ SpringBootForKafka/SpringBootForKafkaProvider/src/main/java/com/kafka/provider/config/KafkaTopicConfig.java
@@ -1,6 +1,7 @@
 package com.kafka.provider.config;
 
 import org.apache.kafka.clients.admin.NewTopic;
+import org.apache.kafka.common.config.TopicConfig;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.TopicBuilder;
 
@@ -11,10 +12,17 @@ import java.util.Map;
 
 public class KafkaTopicConfig {
     public NewTopic generateTopic() {
-        Map<String, String> cofigurations = new HashMap<>();
+        Map<String, String> configurations = new HashMap<>();
+        configurations.put(TopicConfig.CLEANUP_POLICY_COMPACT,TopicConfig.CLEANUP_POLICY_DELETE);  //Delete=>Borrar mensajes, Compact=> Mantiene el mas actual
+        configurations.put(TopicConfig.RETENTION_MS_CONFIG, "86400000");// tiempo de retencion de mensajes  - defecto -1
+        configurations.put(TopicConfig.SEGMENT_BYTES_CONFIG,"1073741824"); //temanio maximo del segmento  - 1GB
+        configurations.put(TopicConfig.MAX_MESSAGE_BYTES_CONFIG, "1000012"); //Tamanio maximo de cada mensaje -defecto 1MB
+
+
         return TopicBuilder.name("NameNewTopic")
                 .partitions(2)
                 .replicas(2)
+                .configs(configurations)
                 .build();
     }
 }
